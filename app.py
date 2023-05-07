cb44c4d860c627fc80f03c01203e90184bda6914
ibrooz94 <ibrooz94@gmail.com>
2023-05-07 18:14:18 +0100

    environ update

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 api = Api(app)
 CORS(app)
